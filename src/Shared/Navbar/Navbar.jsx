7baa2a629b7af1fec300cfd48b0eb93567e4800a
himaloyash <himaloyashis98@gmail.com>
2023-06-28 23:57:48 +0600

    set up

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <li>About</li>
+            <li>Work</li>
+            <li>Get in touch</li>
+        </div>
+    );
+};
+
+export default Navbar;
