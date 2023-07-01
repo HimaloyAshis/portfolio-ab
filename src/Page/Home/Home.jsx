968a594154f8c1eed3b47188294f21c7c12119b2
himaloyash <himaloyashis98@gmail.com>
2023-07-01 23:52:42 +0600

    contact css added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -184,8 +184,8 @@ const Home = () => {
                                     <div className="bar"><span></span></div>
                                 </div>
                                 <div className="progress">
-                                    <h3>jwt <span>75%</span></h3>
-                                    <div className="bar"><span></span></div>
+                                    <h3>JWT <span>75%</span></h3>
+                                    <div className="bar"><span className='ok'></span></div>
                                 </div>
 
                             </div>
@@ -193,6 +193,34 @@ const Home = () => {
                     </div>
                 </div>
 
+            </section>
+
+            {/* contact section */}
+
+            <section className="contact" id="contact">
+
+                <h2 className='heading'>Contact <span>Me!</span></h2>
+
+                <form action="">
+                    <div className="input-box">
+                        <div className="input-field">
+                            <input type="email" name="email" placeholder='fullName' id="" />
+                            <span className='focus'></span>
+                        </div>
+                        <div className="input-field">
+                            <input type="email" name="email" placeholder='fullName' id="" />
+                            <span className='focus'></span>
+                        </div>
+                    </div>
+                    <div className="textarea-field">
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <span className='focus'></span>
+                    </div>
+                    <div className="btn-box btns">
+                        <button type="submit" className='btn'>Submit</button>
+                    </div>
+
+                </form>
             </section>
         </>
     );
